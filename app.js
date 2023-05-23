6c2444c2b0846b1143b1d366a8d5742262ef9b4a
Pita Badila <ritualmomma@gmail.com>
2023-05-23 19:31:05 -0400

    Alters start script to remove seed:all in attempt to resolve render fail, adds cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -18,7 +19,7 @@ var choresRouter = require('./routes/chores');
 
 
 var app = express();
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
